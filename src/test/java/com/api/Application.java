3d9c8548ee6496a5efe97a4a290b07bdff6d2b5c
RaulMafra <raulcesar.sm@gmail.com>
2023-10-24 09:42:40 -0300

    feat: other files

---
+++ src/test/java/com/api/Application.java
@@ -0,0 +1,14 @@
+package com.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+
+}
