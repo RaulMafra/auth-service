394063f0c660591211785c5dfb4f169134505134
RaulMafra <raulcesar.sm@gmail.com>
2023-10-26 15:09:37 -0300

    refactor: Refactored User entity

--- src/main/java/com/api/model/User.java
+++ src/main/java/com/api/model/User.java
@@ -1,92 +1,78 @@
 package com.api.model;
 
-import lombok.NoArgsConstructor;
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
-import javax.persistence.CollectionTable;
-import javax.persistence.Column;
-import javax.persistence.ElementCollection;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.Table;
+import javax.persistence.*;
 
-@NoArgsConstructor
-@Entity
 @Table(name = "tb_user")
+@Entity
 public class User {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id_user")
-	private Long id;
-	@Column(length = 50, nullable = false)
-	private String name;
-	@Column(length = 20, nullable = false)
-	private String username;
-	@Column(length = 100, nullable = false)
-	private String password;
-	@ElementCollection(fetch = FetchType.EAGER)
-	@CollectionTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "id_user"))
-	@Column(name = "role_id")
-	private List<String> role = new ArrayList<>();
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
+    private Long id;
+    @Column(length = 50, nullable = false)
+    private String name;
+    @Column(length = 20, nullable = false)
+    private String username;
+    @Column(length = 100, nullable = false)
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private List<Role> role;
 
-	public User(String name, String username, String password, List<String> role){
-		this.name = name;
-		this.username = username;
-		this.password = password;
-		this.role = role;
-	}
+    public User() {
 
-	public Long getId() {
-		return id;
-	}
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public User(Long id, String name, String username, String password, List<Role> role) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getUsername() {
-		return username;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setUsername(String login) {
-		this.username = login;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getUsername() {
+        return username;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	public List<String> getRole() {
-		return role;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public void setRole(List<String> role) {
-		this.role = role;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", name=" + name + ", username=" + username + ", password=" + password + "]";
-	}
+    public List<Role> getRole() {
+        return role;
+    }
 
+    public void setRole(List<Role> role) {
+        this.role = role;
+    }
 }
