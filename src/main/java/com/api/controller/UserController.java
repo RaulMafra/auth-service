01bea9b0f497960d853ac6cb5834b0bb23097522
RaulMafra <raulcesar.sm@gmail.com>
2023-10-26 15:14:53 -0300

    feat: Generation and Verification of the created token

--- src/main/java/com/api/controller/UserController.java
+++ src/main/java/com/api/controller/UserController.java
@@ -6,10 +6,7 @@ import com.api.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.api.service.UserService;
 
@@ -19,46 +16,40 @@ import java.util.List;
 @RequestMapping("/users")
 public class UserController {
 
-	@Autowired
-	private UserService userService;
+    @Autowired
+    private UserService userService;
 
-	@PostMapping
-	public ResponseEntity<UserResponseDTO> saveUser(@RequestBody UserRequestDTO dto) {
-		User userCreated = userService.saveUser(dto.turnForObject());
-		return new ResponseEntity<>(UserResponseDTO.turnForDTO(userCreated), HttpStatus.CREATED);
-	}
+    @PostMapping("/register")
+    public ResponseEntity<UserResponseDTO> saveUser(@RequestBody UserRequestDTO dto) {
+        User userCreated = userService.saveUser(dto.turnForObject());
+        return new ResponseEntity<>(UserResponseDTO.turnForDTO(userCreated), HttpStatus.CREATED);
+    }
+
+/*    @GetMapping("/{username}")
+    public User getOne(@PathVariable String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @GetMapping
+    public String getUser() {
+        return "Usuário";
+    }
+
+    @GetMapping("/{username}")
+    public String getUserByUsername(@PathVariable String username) {
+        return "Usuário " + username + " encontrado!";
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Integer id) {
+        userRepository.deleteById(id);
+    }
 
 
-	@GetMapping()
-	public List<User> getUsers(){
-		return userRepository.findAll();
-	}
+    @PutMapping()
+    public void updateUser(@RequestBody() User user) {
+        userRepository.save(user);
+    }*/
 
-	@GetMapping("/{username}")
-	public User getOne(@PathVariable String username) {
-		return userRepository.findByUsername(username);
-	}
-	
-	@GetMapping
-	public String getUser() {
-		return "Usuário";
-	}
-	
-	@GetMapping("/{username}")
-	public String getUserByUsername(@PathVariable String username) {
-		return "Usuário " + username + " encontrado!";
-	}
-	
-	@DeleteMapping("/{id}")
-	public void deleteUser(@PathVariable Integer id) {
-		userRepository.deleteById(id);
-	}
-	
-	
-	@PutMapping()
-	public void updateUser(@RequestBody() User user) {
-		userRepository.save(user);
-	}
-	
-	
+
 }
