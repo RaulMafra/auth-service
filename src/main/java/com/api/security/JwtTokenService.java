14a27e6e47b0c0b9404a93e60128ca97319a8fa4
RaulMafra <raulcesar.sm@gmail.com>
2023-10-26 15:15:55 -0300

    feat: Generation and Verification of the created token

---
+++ src/main/java/com/api/security/JwtTokenService.java
@@ -0,0 +1,52 @@
+package com.api.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+public class JwtTokenService {
+
+    private final String SECRET_KEY = "AbIJTDMFc7yUa5MhvcP03nJPyCPzZtQcGEp-zWfOkEE";
+    private final String ISSUER = "login-service-api";
+
+    public String generationToken(UserDetailsImpl user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
+            return JWT.create()
+                    .withIssuer(ISSUER)
+                    .withIssuedAt(creationDate())
+                    .withExpiresAt(expirationDate())
+                    .withSubject(user.getUsername())
+                    .sign(algorithm);
+        } catch (JWTCreationException e) {
+            throw new JWTCreationException("Error to a generate token.", e);
+        }
+    }
+
+    public String verificationToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
+            return JWT.require(algorithm)
+                    .withIssuer(ISSUER)
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException e) {
+            throw new JWTVerificationException("Invalid token or expirated.");
+        }
+    }
+
+    public Instant creationDate() {
+        return LocalDateTime.now().toInstant(ZoneOffset.UTC);
+    }
+
+    public Instant expirationDate() {
+        return LocalDateTime.now().plusHours(3).toInstant(ZoneOffset.UTC);
+
+    }
+}
