0035d1bbf9fa9d6d0a741bf93615edcb2bc7cc73
RaulMafra <raulcesar.sm@gmail.com>
2023-10-26 15:10:08 -0300

    refactor: Refactored interface UserRepository

--- src/main/java/com/api/repository/UserRepository.java
+++ src/main/java/com/api/repository/UserRepository.java
@@ -5,11 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-	@Query("SELECT e FROM User e JOIN FETCH e.role WHERE e.username = (:username)")
-	 User findByUsername(@Param("username") String username);
-	
+//	@Query("SELECT e FROM User e JOIN FETCH e.role WHERE e.username = (:username)")
+//	 User findByUsername(@Param("username") String username);
+
+    Optional<User> findByUsername(String username);
+
 //	boolean existByUsername(String username);
 
 }
