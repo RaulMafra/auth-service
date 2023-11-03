f75d684840a575358dc7f94db5d9ff2c0dc7387c
RaulMafra <raulcesar.sm@gmail.com>
2023-11-02 21:56:10 -0300

    feat: Message of register "ok" added

--- src/main/java/com/auth/controller/UserController.java
+++ src/main/java/com/auth/controller/UserController.java
@@ -1,55 +1,41 @@
-package com.api.controller;
+package com.auth.controller;
 
-import com.api.dto.UserRequestDTO;
-import com.api.dto.UserResponseDTO;
-import com.api.model.User;
+import com.auth.dto.AuthUserDTO;
+import com.auth.dto.RegisterUserDTO;
+import com.auth.dto.RetriveJWTTokenDTO;
+import com.auth.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.api.service.UserService;
-
-import java.util.List;
-
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<UserResponseDTO> saveUser(@RequestBody UserRequestDTO dto) {
-        User userCreated = userService.saveUser(dto.turnForObject());
-        return new ResponseEntity<>(UserResponseDTO.turnForDTO(userCreated), HttpStatus.CREATED);
+    public ResponseEntity<String> createUser(@RequestBody RegisterUserDTO createUserDTO) {
+        userService.createUser(createUserDTO);
+        return new ResponseEntity<>("User created: Ok", HttpStatus.CREATED);
     }
 
-/*    @GetMapping("/{username}")
-    public User getOne(@PathVariable String username) {
-        return userRepository.findByUsername(username);
+    @PostMapping("/login")
+    public ResponseEntity<RetriveJWTTokenDTO> authenticateUser(@RequestBody AuthUserDTO authUserDTO) {
+        RetriveJWTTokenDTO token = userService.authenticateUser(authUserDTO);
+        return new ResponseEntity<>(token, HttpStatus.OK);
     }
 
-    @GetMapping
-    public String getUser() {
-        return "Usuário";
+    @GetMapping("/myUser")
+    public ResponseEntity<String> commonUser() {
+        return new ResponseEntity<>("Usuário comum autorizado!", HttpStatus.OK);
     }
 
-    @GetMapping("/{username}")
-    public String getUserByUsername(@PathVariable String username) {
-        return "Usuário " + username + " encontrado!";
+    @GetMapping("/listUsers")
+    public ResponseEntity<String> admin() {
+        return new ResponseEntity<>("Administrador autorizado!", HttpStatus.OK);
     }
-
-    @DeleteMapping("/{id}")
-    public void deleteUser(@PathVariable Integer id) {
-        userRepository.deleteById(id);
-    }
-
-
-    @PutMapping()
-    public void updateUser(@RequestBody() User user) {
-        userRepository.save(user);
-    }*/
-
 
 }
