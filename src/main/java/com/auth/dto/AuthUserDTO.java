c8f468d9d799fcbde01f1fe207fb24a66e0ff995
RaulMafra <raulcesar.sm@gmail.com>
2023-11-05 00:16:15 -0300

    refactor: "List of the roles removed"

--- src/main/java/com/auth/dto/AuthUserDTO.java
+++ src/main/java/com/auth/dto/AuthUserDTO.java
@@ -1,7 +1,3 @@
 package com.auth.dto;
 
-import com.auth.model.Role;
-
-import java.util.List;
-
-public record AuthUserDTO(String username, String password, List<Role> role) { }
+public record AuthUserDTO(String username, String password) { }
