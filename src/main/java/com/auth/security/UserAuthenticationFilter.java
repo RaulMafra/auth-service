be6ea7cacca2e6f45f7c9e6e4fb2be8cd3089212
RaulMafra <raulcesar.sm@gmail.com>
2023-11-14 15:09:58 -0300

    refactor: Changed the query statement of the username for id

--- src/main/java/com/auth/security/UserAuthenticationFilter.java
+++ src/main/java/com/auth/security/UserAuthenticationFilter.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -30,39 +29,31 @@ public class UserAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        //Verifica se o endpoint requer autenticação antes de processar a requisição
         if (checkIfEndpointIsNotPublic(request)) {
-            String token = recoveryToken(request); //Recupera o token do header Authorization da requisição
+            String token = recoveryToken(request);
             if (token != null) {
-                String subject = jwtTokenService.verificationToken(token); //Retorna o assunto(username) do token verificado.
-//                User user = userRepository.findByUsername(subject).get(); //Busca o usuário pelo username
-//                UserDetailsImpl userDetails = new UserDetailsImpl(user); //Cria um UserDetails com base no usuário encontrado
-                //Inicializa um objeto de autenticação com o username e a(s) role(s) do usuário encontrado
-                UserDetailsImpl userDetails = new UserDetailsImpl(userRepository.findByUsername(subject).orElseThrow(() ->
-                        new UsernameNotFoundException("Usuário não encontrado")));
+                String subject = jwtTokenService.verificationToken(token);
+                UserDetailsImpl userDetails = new UserDetailsImpl(userRepository.findById(Long.parseLong(String.valueOf(request.getRequestURI().charAt(6)))).get());
                 Authentication authenticaton =
                         new UsernamePasswordAuthenticationToken(userDetails.getUsername(), null, userDetails.getAuthorities());
-                //Define o objeto de autenticação no contexto de segurança do Spring Security
                 SecurityContextHolder.getContext().setAuthentication(authenticaton);
             } else {
                 throw new BusinessException("O token está incorreto ou foi perdido");
             }
         }
-        filterChain.doFilter(request, response); //Continua o processamento da requisição
+        filterChain.doFilter(request, response);
     }
 
-    //Recupera o token do cabeçalho Authorization da requisição
     private String recoveryToken(HttpServletRequest request) {
-        String authorizationHeader = request.getHeader("Authorization"); //Acessa o valor "Authorization" do header da requisição enviada pelo client
+        String authorizationHeader = request.getHeader("Authorization");
         if (authorizationHeader != null) {
             return authorizationHeader.replace("Bearer ", "");
         }
         return null;
     }
 
-    //Verifica se o recurso atual é privado, isto é, verifica se requer autenticação antes de processar a requisição.
     private boolean checkIfEndpointIsNotPublic(HttpServletRequest request) {
-        String requestURI = request.getRequestURI(); //Obtém o endpoint chamado na requisição pelo client
+        String requestURI = request.getRequestURI();
         List<String> accessAllowed = Arrays.asList(SecurityConfiguration.RESOURCES_WITH_AUTHENTICATION_NOT_REQUIRED);
         if(!requestURI.contains("/favicon.ico")) return !accessAllowed.stream().anyMatch(requestURI::startsWith);
         else return false;
