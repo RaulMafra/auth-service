549ff5a2c04cb17121db40e46faac82693fa00d9
RaulMafra <raulcesar.sm@gmail.com>
2023-11-14 15:04:11 -0300

    feat: Statement for update the user

--- src/main/java/com/auth/repository/UserRepository.java
+++ src/main/java/com/auth/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.auth.repository;
 
 import com.auth.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -9,5 +11,11 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
 
-
+    @Modifying
+    @Query(nativeQuery = true, value = """
+            UPDATE tb_user
+            SET name = :name, username = :username, password = :password
+            WHERE id_user = :id
+            """)
+    void updateUser(Long id, String name, String username, String password);
 }
