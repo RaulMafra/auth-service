93cf3f77d2507bd4f5a91af729da077cddee88cf
RaulMafra <raulcesar.sm@gmail.com>
2023-11-14 15:03:06 -0300

    feat: Delete method and update method added

--- src/main/java/com/auth/service/UserService.java
+++ src/main/java/com/auth/service/UserService.java
@@ -3,6 +3,7 @@ package com.auth.service;
 import com.auth.dto.AuthUserDTO;
 import com.auth.dto.RegisterUserDTO;
 import com.auth.dto.RetriveJWTTokenDTO;
+import com.auth.dto.UpdateUserDTO;
 import com.auth.handler.BusinessException;
 import com.auth.model.Role;
 import com.auth.model.User;
@@ -15,6 +16,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Objects;
@@ -49,11 +51,21 @@ public class UserService {
 
     public void createUser(RegisterUserDTO createUserDTO) {
         if(Stream.of(createUserDTO.name(), createUserDTO.username(), createUserDTO.password(), createUserDTO.role()).anyMatch(Objects::isNull)){
-            throw new BusinessException("Há algum campo vazio");
+            throw new BusinessException("Há algum campo vazio ou incorreto");
         }
         User newUser = new User(createUserDTO.name(), createUserDTO.username(), securityConfiguration.passwordEncoder().encode(createUserDTO.password())
                 ,List.of(new Role(createUserDTO.role())));
         userRepository.save(newUser);
+    }
 
+    public void delete(Long id) {
+        User user = userRepository.findById(id).get();
+        userRepository.delete(user);
+    }
+
+    @Transactional
+    public void update(UpdateUserDTO updateUser, Long id) {
+        User user = userRepository.findById(id).get();
+        userRepository.updateUser(user.getId(), updateUser.name(), updateUser.username(), securityConfiguration.passwordEncoder().encode(updateUser.password()));
     }
 }
