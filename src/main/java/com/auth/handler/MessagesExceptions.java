2e5c03ce3e99b31d6ec0e01b9ac4bb86a5ab24d2
RaulMafra <raulcesar.sm@gmail.com>
2023-11-20 23:24:54 -0300

    feat: Created class to couple the exceptions messages of the *.properties file

---
+++ src/main/java/com/auth/handler/MessagesExceptions.java
@@ -0,0 +1,10 @@
+package com.auth.handler;
+
+public class MessagesExceptions {
+
+    public static final String USER_NOT_FOUND_OR_INVALID_PASSWORD = "exception.userNotFound.invalidPassword";
+    public static final String EXCEPTION_UNEXPECTED = "exception.unexpected";
+    public static final String EXCEPTION_BLANK_FIELD = "exception.blank.field";
+    public static final String USER_NOT_FOUND = "exception.userNotFound";
+    public static final String JWT_VERIFICATION_ERROR = "exception.jwt.verification.error";
+}
