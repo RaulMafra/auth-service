2a459f1fa748099a4c8095373fda34dac13f0c30
RaulMafra <raulcesar.sm@gmail.com>
2023-11-23 10:29:15 -0300

    refactor: Refactored the null field validator

--- src/main/java/com/auth/service/UserService.java
+++ src/main/java/com/auth/service/UserService.java
@@ -4,15 +4,14 @@ import com.auth.dto.AuthUserDTO;
 import com.auth.dto.RegisterUserDTO;
 import com.auth.dto.RetriveJWTTokenDTO;
 import com.auth.dto.UpdateUserDTO;
-import com.auth.handler.BusinessException;
 import com.auth.handler.MessagesExceptions;
 import com.auth.model.Role;
-import com.auth.model.RoleName;
 import com.auth.model.User;
 import com.auth.repository.UserRepository;
 import com.auth.security.JwtTokenService;
 import com.auth.security.UserDetailsImpl;
 import com.auth.security.config.SecurityConfiguration;
+import com.auth.util.FieldsValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -21,10 +20,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Stream;
 
 @Service
 public class UserService {
@@ -42,9 +38,7 @@ public class UserService {
     private AuthenticationManager authenticationManager;
 
     public RetriveJWTTokenDTO authenticateUser(AuthUserDTO authUserDTO) {
-        if(Stream.of(authUserDTO.username(), authUserDTO.password()).anyMatch(Objects::isNull)){
-            throw new BusinessException(MessagesExceptions.EXCEPTION_BLANK_FIELD);
-        }
+        FieldsValidator.checkNullAuthUserDTO(authUserDTO);
         UsernamePasswordAuthenticationToken usernamePassword = new
                 UsernamePasswordAuthenticationToken(authUserDTO.username(), authUserDTO.password());
         Authentication authentication = authenticationManager.authenticate(usernamePassword);
@@ -52,15 +46,10 @@ public class UserService {
         return new RetriveJWTTokenDTO(jwtTokenService.generationToken((UserDetailsImpl) authentication.getPrincipal()));
     }
 
-    public void createUser(RegisterUserDTO createUserDTO) {
-        if(Stream.of(createUserDTO.name(), createUserDTO.username(), createUserDTO.password(), createUserDTO.role()).anyMatch(Objects::isNull)){
-            throw new BusinessException(MessagesExceptions.EXCEPTION_BLANK_FIELD);
-        }
-        if(!Arrays.stream(RoleName.values()).anyMatch(role -> role.equals(createUserDTO.role()))){
-            throw new RuntimeException("Role inválida");
-        }
-        User newUser = new User(createUserDTO.name(), createUserDTO.username(), securityConfiguration.passwordEncoder().encode(createUserDTO.password())
-                ,List.of(new Role(createUserDTO.role())));
+    public void RegisterUser(RegisterUserDTO registerUserDTO) {
+        FieldsValidator.checkNullRegisterUserDTO(registerUserDTO);
+        User newUser = new User(registerUserDTO.name(), registerUserDTO.username(), securityConfiguration.passwordEncoder().encode(registerUserDTO.password())
+                ,List.of(new Role(registerUserDTO.role())));
         userRepository.save(newUser);
     }
 
@@ -71,6 +60,7 @@ public class UserService {
 
     @Transactional
     public void update(UpdateUserDTO updateUser, String username) {
+        FieldsValidator.checkNullUpdateUserDTO(updateUser);
         User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(MessagesExceptions.USER_NOT_FOUND));
         userRepository.updateUser(user.getId(), updateUser.name(), updateUser.username(), securityConfiguration.passwordEncoder().encode(updateUser.password()));
     }
