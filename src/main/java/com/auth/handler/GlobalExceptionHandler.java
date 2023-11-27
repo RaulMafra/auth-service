e44bffccef6eaf11460ca7b2a60f7ec688c2676e
RaulMafra <raulcesar.sm@gmail.com>
2023-11-27 00:11:34 -0300

    feat: Added exception handling for register with username already existence

--- src/main/java/com/auth/handler/GlobalExceptionHandler.java
+++ src/main/java/com/auth/handler/GlobalExceptionHandler.java
@@ -2,9 +2,11 @@ package com.auth.handler;
 
 import com.auth.handler.exceptions.BusinessException;
 import com.auth.handler.exceptions.CheckFieldsException;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -39,53 +41,59 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler(BusinessException.class)
-    private ResponseEntity<Object> handleBusinessException(BusinessException e, WebRequest request) {
-        if(e.getClass().isAssignableFrom(BusinessException.class)) {
-            String errorMessage = messageSource().getMessage(e.getMessage(), null, Locale.US);
-            ResponseError responseError = new ResponseError(HttpStatus.I_AM_A_TEAPOT.value(), errorMessage);
+    private ResponseEntity<Object> handleBusinessException(BusinessException e, WebRequest request, HttpServletRequest httpRequest) {
+        if (e.getClass().isAssignableFrom(BusinessException.class)) {
+            String message = messageSource().getMessage(e.getMessage(), null, Locale.US);
+            ResponseError responseError = new ResponseError(HttpStatus.I_AM_A_TEAPOT.value(), HttpStatus.I_AM_A_TEAPOT, message, httpRequest.getRequestURI());
             return createResponseEntity(responseError, headers(), HttpStatus.I_AM_A_TEAPOT, request);
         }
         if (e.getClass().isAssignableFrom(CheckFieldsException.class)) {
-            String errorMessage = messageSource().getMessage(e.getMessage(), null, Locale.US);
-            ResponseError responseError = new ResponseError(HttpStatus.BAD_REQUEST.value(), errorMessage);
+            String message = messageSource().getMessage(e.getMessage(), null, Locale.US);
+            ResponseError responseError = new ResponseError(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST, message, httpRequest.getRequestURI());
             return createResponseEntity(responseError, headers(), HttpStatus.BAD_REQUEST, request);
         }
-        return handleGeneral(e, request);
+        return handleGeneral(e, request, httpRequest);
     }
 
     @ExceptionHandler(AuthenticationException.class)
-    private ResponseEntity<Object> handleAuthenticationFail(AuthenticationException e, WebRequest request) {
+    private ResponseEntity<Object> handleAuthenticationFail(AuthenticationException e, WebRequest request, HttpServletRequest httpRequest) {
         if (e.getClass().isAssignableFrom(BadCredentialsException.class)) {
             AuthenticationException exception = new BadCredentialsException(MessagesExceptions.USER_NOT_FOUND_OR_INVALID_PASSWORD);
-            String errorMessage = messageSource().getMessage(exception.getMessage(), null, Locale.US);
-            ResponseError responseError = new ResponseError(HttpStatus.BAD_REQUEST.value(), errorMessage);
-            return createResponseEntity(responseError, headers(), HttpStatus.BAD_REQUEST, request);
+            String message = messageSource().getMessage(exception.getMessage(), null, Locale.US);
+            ResponseError responseError = new ResponseError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED, message, httpRequest.getRequestURI());
+            return createResponseEntity(responseError, headers(), HttpStatus.UNAUTHORIZED, request);
         }
         if (e.getClass().isAssignableFrom(InternalAuthenticationServiceException.class)) {
             AuthenticationException exception = new InternalAuthenticationServiceException(MessagesExceptions.USER_NOT_FOUND_OR_INVALID_PASSWORD);
-            String errorMessage = messageSource().getMessage(exception.getMessage(), null, Locale.US);
-            ResponseError responseError = new ResponseError(HttpStatus.BAD_REQUEST.value(), errorMessage);
-            return createResponseEntity(responseError, headers(), HttpStatus.BAD_REQUEST, request);
+            String message = messageSource().getMessage(exception.getMessage(), null, Locale.US);
+            ResponseError responseError = new ResponseError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED, message, httpRequest.getRequestURI());
+            return createResponseEntity(responseError, headers(), HttpStatus.UNAUTHORIZED, request);
         }
         if (e.getClass().isAssignableFrom(UsernameNotFoundException.class)) {
-            AuthenticationException exception = new UsernameNotFoundException(MessagesExceptions.USER_NOT_FOUND);
-            String errorMessage = messageSource().getMessage(exception.getMessage(), null, Locale.US);
-            ResponseError responseError = new ResponseError(HttpStatus.NOT_FOUND.value(), errorMessage);
+            AuthenticationException exception = new UsernameNotFoundException(e.getMessage());
+            String message = messageSource().getMessage(exception.getMessage(), null, Locale.US);
+            String[] nameUser = httpRequest.getRequestURI().split("/");
+            ResponseError responseError = new ResponseError(HttpStatus.NOT_FOUND.value(), HttpStatus.NOT_FOUND, message.concat(" " + nameUser[2]), httpRequest.getRequestURI());
             return createResponseEntity(responseError, headers(), HttpStatus.NOT_FOUND, request);
         }
-        ResponseError responseError = new ResponseError(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
-        e.printStackTrace();
-        return createResponseEntity(responseError, headers(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+        return handleGeneral(e, request, httpRequest);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    private ResponseEntity<Object> handleDatasPersistence(DataIntegrityViolationException e, WebRequest request, HttpServletRequest httpRequest) {
+        DataIntegrityViolationException exception = new DataIntegrityViolationException(MessagesExceptions.INTEGRITY_VIOLATION);
+        String message = messageSource().getMessage(exception.getMessage(), null, Locale.US);
+        ResponseError responseError = new ResponseError(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST, message, httpRequest.getRequestURI());
+        return createResponseEntity(responseError, headers(), HttpStatus.BAD_REQUEST, request);
     }
 
 
     @ExceptionHandler(Exception.class)
-    private ResponseEntity<Object> handleGeneral(Exception e, WebRequest request) {
-        String errorMessage = messageSource().getMessage(MessagesExceptions.EXCEPTION_UNEXPECTED, null, Locale.US);
-        ResponseError responseError = new ResponseError(HttpStatus.INTERNAL_SERVER_ERROR.value(), errorMessage);
+    private ResponseEntity<Object> handleGeneral(Exception e, WebRequest request, HttpServletRequest httpRequest) {
+        String message = messageSource().getMessage(MessagesExceptions.EXCEPTION_UNEXPECTED, null, Locale.US);
+        ResponseError responseError = new ResponseError(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR, message, httpRequest.getRequestURI());
         e.printStackTrace();
         return createResponseEntity(responseError, headers(), HttpStatus.INTERNAL_SERVER_ERROR, request);
     }
-
-
 }
+
