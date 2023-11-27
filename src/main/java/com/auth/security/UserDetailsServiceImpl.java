1a2887853152987d1b5576c10156b002e340a0a5
RaulMafra <raulcesar.sm@gmail.com>
2023-11-27 00:22:23 -0300

    Empty commit

--- src/main/java/com/auth/security/UserDetailsServiceImpl.java
+++ src/main/java/com/auth/security/UserDetailsServiceImpl.java
@@ -4,7 +4,6 @@ import com.auth.handler.MessagesExceptions;
 import com.auth.model.User;
 import com.auth.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
