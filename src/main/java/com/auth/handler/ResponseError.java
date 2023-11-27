0676872beebe0d99a68209c4497d2ac5f4f23080
RaulMafra <raulcesar.sm@gmail.com>
2023-11-27 00:17:45 -0300

    feat: ResponseError class refactored e added the path where the error occurred

--- src/main/java/com/auth/handler/ResponseError.java
+++ src/main/java/com/auth/handler/ResponseError.java
@@ -1,13 +1,24 @@
 package com.auth.handler;
 
+import org.springframework.http.HttpStatus;
+
 import java.time.LocalDateTime;
 
 public class ResponseError {
 
-	private LocalDateTime timestamp = LocalDateTime.now();
-	private String status;
-	private int statusCode;
-	private String error;
+	private LocalDateTime timestamp;
+	private int status;
+	private HttpStatus error;
+	private String message;
+	private String path;
+
+	public ResponseError(int status, HttpStatus error, String message, String path) {
+		this.timestamp = LocalDateTime.now();
+		this.status = status;
+		this.error = error;
+		this.message = message;
+		this.path = path;
+	}
 
 	public LocalDateTime getTimestamp() {
 		return timestamp;
@@ -17,28 +28,36 @@ public class ResponseError {
 		this.timestamp = timestamp;
 	}
 
-	public String getStatus() {
-		return status;
-	}
+    public int getStatus(){
+        return status;
+    }
 
-	public void setStatus(String status) {
+	public void setStatus(int status) {
 		this.status = status;
 	}
 
-	public int getStatusCode() {
-		return statusCode;
-	}
-
-	public void setStatusCode(int statusCode) {
-		this.statusCode = statusCode;
-	}
-
-	public String getError() {
+	public HttpStatus getError(){
 		return error;
 	}
 
-	public void setError(String error) {
+	public void setError(HttpStatus error) {
 		this.error = error;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getPath(){
+		return path;
+	}
+
+	public void setPath(String path){
+		this.path = path;
 	}
 
 }
