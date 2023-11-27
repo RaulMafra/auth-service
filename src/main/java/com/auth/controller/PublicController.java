3fc7b2e2e3ecc299abc4e2ee9c14e8434b4035a8
RaulMafra <raulcesar.sm@gmail.com>
2023-11-27 15:12:07 -0300

    refactor: Created a class for endpoints that do not require authentication

---
+++ src/main/java/com/auth/controller/PublicController.java
@@ -0,0 +1,35 @@
+package com.auth.controller;
+
+import com.auth.dto.AuthUserDTO;
+import com.auth.dto.RegisterUserDTO;
+import com.auth.dto.ResponseDTO;
+import com.auth.dto.RetriveJWTTokenDTO;
+import com.auth.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/restservice/v1")
+public class PublicController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/sign-up")
+    public ResponseEntity<ResponseDTO> createUser(@RequestBody RegisterUserDTO registerUserDTO) {
+        userService.RegisterUser(registerUserDTO);
+        return new ResponseEntity<>(new ResponseDTO("OK"), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/sign-in")
+    public ResponseEntity<RetriveJWTTokenDTO> authenticateUser(@RequestBody AuthUserDTO authUserDTO) {
+        RetriveJWTTokenDTO token = userService.authenticateUser(authUserDTO);
+        return new ResponseEntity<>(token, HttpStatus.OK);
+    }
+
+}
