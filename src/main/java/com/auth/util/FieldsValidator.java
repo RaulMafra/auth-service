df8757e3c52c539248cb4ba9bcde48779afe5f6d
RaulMafra <raulcesar.sm@gmail.com>
2023-11-28 15:10:39 -0300

    feat: Added checks of the null fields

--- src/main/java/com/auth/util/FieldsValidator.java
+++ src/main/java/com/auth/util/FieldsValidator.java
@@ -6,25 +6,34 @@ import com.auth.dto.UpdateUserDTO;
 import com.auth.handler.exceptions.CheckFieldsException;
 import com.auth.handler.MessagesExceptions;
 
+import java.util.List;
 import java.util.Objects;
 import java.util.stream.Stream;
 
 public class FieldsValidator {
 
-    public static void checkNullAuthUserDTO(AuthUserDTO authUserDTO) {
+    public static void checkNullFieldsAuthUserDTO(AuthUserDTO authUserDTO) {
         if (Stream.of(authUserDTO.username(), authUserDTO.password()).anyMatch(Objects::isNull)) {
             throw new CheckFieldsException(MessagesExceptions.EXCEPTION_BLANK_FIELD);
         }
     }
 
-    public static void checkNullRegisterUserDTO(RegisterUserDTO registerUserDTO) {
-        if (Stream.of(registerUserDTO.name(), registerUserDTO.username(), registerUserDTO.password(), registerUserDTO.role()).anyMatch(Objects::isNull)) {
+    public static void checkNullFieldsRegisterUserDTO(List<RegisterUserDTO> registerUserDTO) {
+        registerUserDTO.stream().forEach(user -> {
+            if (Stream.of(user.name(), user.username(), user.password(), user.role()).anyMatch(Objects::isNull)){
+                throw new CheckFieldsException(MessagesExceptions.EXCEPTION_BLANK_FIELD);
+            }
+        });
+    }
+
+    public static void checkNullFieldsUpdateUserDTO(UpdateUserDTO updateUserDTO){
+        if(Stream.of(updateUserDTO.name(), updateUserDTO.username(), updateUserDTO.password()).anyMatch(Objects::isNull)){
             throw new CheckFieldsException(MessagesExceptions.EXCEPTION_BLANK_FIELD);
         }
     }
 
-    public static void checkNullUpdateUserDTO(UpdateUserDTO updateUserDTO){
-        if(Stream.of(updateUserDTO.name(), updateUserDTO.username(), updateUserDTO.password()).anyMatch(Objects::isNull)){
+    public static void checkNullFieldMyUser(String username){
+        if(username == null){
             throw new CheckFieldsException(MessagesExceptions.EXCEPTION_BLANK_FIELD);
         }
     }
