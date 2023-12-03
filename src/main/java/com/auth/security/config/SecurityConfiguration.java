85654e2514ed267eb3a2cbe750f452a6813433ea
RaulMafra <raulcesar.sm@gmail.com>
2023-12-03 14:43:06 -0300

    Empty commit

--- src/main/java/com/auth/security/config/SecurityConfiguration.java
+++ src/main/java/com/auth/security/config/SecurityConfiguration.java
@@ -1,7 +1,6 @@
 package com.auth.security.config;
 
 import com.auth.security.UserAuthenticationFilter;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,8 +14,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.security.web.util.matcher.RequestMatcher;
-import org.springframework.util.AntPathMatcher;
 
 @Configuration
 @EnableWebSecurity
