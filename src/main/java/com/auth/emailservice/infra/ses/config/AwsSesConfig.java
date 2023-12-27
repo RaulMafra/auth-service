87632ae6205e6463ba8601a61c89d2c52ec110e5
RaulMafra <raulcesar.sm@gmail.com>
2023-12-27 14:41:01 -0300

    Empty commit

--- src/main/java/com/auth/emailservice/infra/ses/config/AwsSesConfig.java
+++ src/main/java/com/auth/emailservice/infra/ses/config/AwsSesConfig.java
@@ -4,47 +4,18 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import com.auth.configs.ses.SesConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(value = "aws")
 public class AwsSesConfig {
-
-    private String ACCESS_KEY_ID;
-    private String SECRET_KEY;
-    private String REGION;
-
-    public String getACCESS_KEY_ID() {
-        return ACCESS_KEY_ID;
-    }
-
-    public void setACCESS_KEY_ID(String ACCESS_KEY_ID) {
-        this.ACCESS_KEY_ID = ACCESS_KEY_ID;
-    }
-
-    public String getSECRET_KEY() {
-        return SECRET_KEY;
-    }
-
-    public void setSECRET_KEY(String SECRET_KEY) {
-        this.SECRET_KEY = SECRET_KEY;
-    }
-
-    public String getREGION() {
-        return REGION;
-    }
-
-    public void setREGION(String REGION) {
-        this.REGION = REGION;
-    }
 
     @Bean
     public AmazonSimpleEmailService amazonSimpleEmailService() {
-        BasicAWSCredentials credentials = new BasicAWSCredentials(ACCESS_KEY_ID, SECRET_KEY);
+        BasicAWSCredentials credentials = new BasicAWSCredentials(SesConfig.ACCESS_KEY_ID, SesConfig.SECRET_KEY);
         return AmazonSimpleEmailServiceClientBuilder.standard()
-                .withRegion(REGION)
+                .withRegion(SesConfig.REGION)
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .build();
 
