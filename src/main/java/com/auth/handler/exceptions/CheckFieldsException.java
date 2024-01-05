8d54ab37575c8d3d264ff9f2c2554faad9692c8e
RaulMafra <raulcesar.sm@gmail.com>
2024-01-05 09:23:17 -0300

    refactor: Exceptions refactored

--- src/main/java/com/auth/handler/exceptions/CheckFieldsException.java
+++ src/main/java/com/auth/handler/exceptions/CheckFieldsException.java
@@ -1,6 +1,6 @@
 package com.auth.handler.exceptions;
 
-public class CheckFieldsException extends BusinessException {
+public class CheckFieldsException extends RuntimeException {
 
     public CheckFieldsException(String msg){
         super(msg);
